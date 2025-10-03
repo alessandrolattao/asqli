e41fc8d255b359b3680703813132ef89abee00e0
Alessandro Lattao <me@alessandrolattao.com>
2025-10-04 01:37:05 +0200

    fix: use a variable for the version

--- cmd/sqlai/version.go
+++ cmd/sqlai/version.go
@@ -3,4 +3,4 @@ package main
 // Version is the current version of sqlai
 // This can be overridden at build time with:
 // go build -ldflags="-X main.Version=X.Y.Z" -o sqlai ./cmd/sqlai
-const Version = "0.1.0"
+var Version = "0.1.0"
